51a31638ed256f369c55ccc2b5cf9cbcd60742b1
John Wiegley <johnw@newartisans.com>
2020-04-07 13:55:48 -0700

    Use optparse-applicative

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
@@ -6,19 +7,94 @@ import Control.Applicative
 import Control.Monad (forM_)
 import Data.Aeson
 import Data.ByteString.Lazy as BL
+import Data.Data (Data)
 import Data.Ledger as Ledger
 import Data.Text as T
 import Data.Text.IO as T
+import Data.Typeable (Typeable)
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
+import Options.Applicative
 import Servant.Client
-import System.Environment
 import ThinkOrSwim.API
 import ThinkOrSwim.API.TransactionHistory.GetTransactions as API
 import ThinkOrSwim.Convert
 
-histSampleData :: FilePath
-histSampleData = "/Users/johnw/Documents/accounts/broker/Ameritrade/hist.json"
+version :: String
+version = "0.0.1"
+
+copyright :: String
+copyright = "2020"
+
+thinkorswimSummary :: String
+thinkorswimSummary =
+    "thinkorswim " ++ version ++ ", (C) " ++ copyright ++ " John Wiegley"
+
+data Options = Options
+    { dryRun    :: Bool
+    , verbose   :: Bool
+    , startDate :: Maybe String
+    , endDate   :: Maybe String
+    , account   :: String
+    , accessKey :: Maybe String
+    , jsonData  :: Maybe FilePath
+    }
+    deriving (Data, Typeable, Show, Eq)
+
+thinkorswimOpts :: Parser Main.Options
+thinkorswimOpts = Main.Options
+    <$> switch
+        (   short 'n'
+         <> long "dry-run"
+         <> help "Don't take any actions")
+    <*> switch
+        (   short 'v'
+         <> long "verbose"
+         <> help "Report progress verbosely")
+    <*> optional (strOption
+        (   long "start-date"
+         <> help "download transactions starting from this date, YYYY-mm-dd format"))
+    <*> optional (strOption
+        (   long "end-date"
+         <> help "download transactions up to this date, YYYY-mm-dd format"))
+    <*> strOption
+        (   long "account"
+         <> help "account number to download for")
+    <*> optional (strOption
+        (   long "access-key"
+         <> help "Use a specific ssh command"))
+    <*> optional (strOption
+        (   long "json-data"
+         <> help "JSON file containing already downloaded data"))
+
+optionsDefinition :: ParserInfo Main.Options
+optionsDefinition = info
+    (helper <*> thinkorswimOpts)
+    (fullDesc <> progDesc "" <> header thinkorswimSummary)
+
+getOptions :: IO Main.Options
+getOptions = execParser optionsDefinition
+
+main :: IO ()
+main = do
+    opts <- getOptions
+
+    th <- case accessKey opts of
+        Nothing -> case jsonData opts of
+            Nothing   -> error "Neither --access-key nor --json-data provided"
+            Just file -> readTransactions file
+        Just key ->
+            downloadTransactions (T.pack (Main.account opts)) (T.pack key)
+                =<< createManager
+
+    -- Prelude.putStrLn "--- Data read from JSON ---"
+    -- pPrint th
+
+    -- Prelude.putStrLn "--- Data converted to Ledger format ---"
+    forM_ (convertTransactions th) $ \t -> do
+        forM_ (renderTransaction t)
+            T.putStrLn
+        T.putStrLn ""
 
 createManager :: IO Manager
 createManager =
@@ -55,24 +131,5 @@ downloadTransactions actId accessToken mgr = do
         , baseUrlPath   = "/v1"
         }
 
-readTransactions :: IO TransactionHistory
-readTransactions = do
-    eres <- eitherDecode <$> BL.readFile histSampleData
-    case eres of
-        Left err -> error err
-        Right th -> pure th
-
-main :: IO ()
-main = do
-    th <- readTransactions <|> do
-        act:token:_ <- Prelude.map T.pack <$> getArgs
-        downloadTransactions act token =<< createManager
-
-    -- Prelude.putStrLn "--- Data read from JSON ---"
-    -- pPrint th
-
-    -- Prelude.putStrLn "--- Data converted to Ledger format ---"
-    forM_ (convertTransactions th) $ \t -> do
-        forM_ (renderTransaction t)
-            T.putStrLn
-        T.putStrLn ""
+readTransactions :: FilePath -> IO TransactionHistory
+readTransactions fp = either fail pure =<< eitherDecode <$> BL.readFile fp
