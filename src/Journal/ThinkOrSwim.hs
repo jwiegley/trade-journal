235439d7ef72a9b61fd484b6d02c2f21aae82e99
John Wiegley <johnw@newartisans.com>
2020-11-13 21:15:09 -0800

    More work on parsing TD CSV files

--- src/Journal/ThinkOrSwim.hs
+++ src/Journal/ThinkOrSwim.hs
@@ -14,14 +14,18 @@ import qualified Data.ByteString as B
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Csv as Csv
+import Data.Foldable
 import Data.Map (Map)
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TL
--- import Data.Time
-import Data.Vector (toList)
--- import Journal.Amount
+import Data.Void (Void)
+import GHC.TypeLits (KnownNat)
+import Journal.Amount
 import System.IO
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 
 data ThinkOrSwim = ThinkOrSwim
   { _account :: Text,
@@ -38,6 +42,7 @@ data ThinkOrSwim = ThinkOrSwim
         B.ByteString
         ([Csv.NamedRecord], [Csv.NamedRecord], [Csv.NamedRecord])
   }
+  deriving (Eq, Show)
 
 makeLenses ''ThinkOrSwim
 
@@ -70,7 +75,7 @@ readCsv path = withFile path ReadMode $ \h -> do
   _futures <- readSection h
   "Forex Statements" <- readLine h
   _forex <- readSection h
-  " " <- readLine h
+  _ <- readLine h
   "\"Total" : "Cash" : _ <-
     TL.words . TL.decodeUtf8 <$> readLine h
   "" <- readLine h
@@ -113,3 +118,189 @@ readCsv path = withFile path ReadMode $ \h -> do
       line <- BL.fromStrict <$> B.hGetLine h
       -- print line
       pure line
+
+type Symbol = Text
+
+data TOSEntry
+  = AchCredit
+  | AchDebit
+  | AdrFee Symbol
+  | Bought TOSDevice TOSTrade
+  | CashAltInterest (Amount 2) Symbol
+  | CourtesyAdjustment
+  | CourtesyCredit
+  | ForeignTaxWithheld Symbol
+  | FundDisbursement
+  | IncomingAccountTransfer
+  | InterestAdjustment
+  | InterestIncome Symbol
+  | MarkToMarket
+  | MiscellaneousJournalEntry
+  | OffCycleInterest Symbol
+  | OrdinaryDividend Symbol
+  | QualifiedDividend Symbol
+  | Rebate
+  | RemoveOptionDueToAssignment (Amount 1) Symbol TOSOption
+  | RemoveOptionDueToExpiration (Amount 1) Symbol TOSOption
+  | Sold TOSDevice TOSTrade
+  | TransferBetweenAccounts
+  | TransferFromForexAccount
+  | TransferInSecurityOrOption
+  | TransferOfCash
+  | TransferToForexAccount
+  | WireIncoming
+  deriving (Eq, Show)
+
+data PutCall = Put | Call
+  deriving (Eq, Show)
+
+data TOSDevice = Desktop | IPhone | IPad
+  deriving (Eq, Show)
+
+data TOSOption = TOSOption
+  { optMultiple :: Amount 0,
+    optExpirationDate :: Amount 0,
+    optKind :: PutCall
+  }
+  deriving (Eq, Show)
+
+data TOSTrade = TOSTrade
+  { tdQuantity :: Amount 0,
+    tdSymbol :: Symbol,
+    tdOptDetails :: [TOSOption],
+    tdPrice :: Amount 0,
+    tdExchange :: Maybe Text
+  }
+  deriving (Eq, Show)
+
+type Parser = ParsecT Void Text Identity
+
+parseTrade :: Parser TOSTrade
+parseTrade = do
+  tdQuantity <- pure 0
+  tdSymbol <- pure ""
+  tdOptDetails <- pure []
+  tdPrice <- pure 0
+  tdExchange <- optional parseExchange
+  pure TOSTrade {..}
+
+parseDevice :: Parser TOSDevice
+parseDevice =
+  (IPhone <$ symbol "tIP")
+    <|> (IPad <$ symbol "tIPAD")
+    <|> pure Desktop
+
+parseAmount :: KnownNat n => Parser (Amount n)
+parseAmount = undefined
+
+parseSymbol :: Parser Symbol
+parseSymbol = TL.pack <$> some (oneOf ['A' .. 'Z'])
+
+parseEntry :: Parser TOSEntry
+parseEntry =
+  (AchCredit <$ symbol "ACH CREDIT RECEIVED")
+    <|> (AchDebit <$ symbol "ACH DEBIT RECEIVED")
+    <|> (AdrFee <$> (symbol "ADR FEE~" *> parseSymbol))
+    <|> (Bought <$> parseDevice <*> (symbol "BOT" *> parseTrade))
+    <|> ( CashAltInterest
+            <$> parseAmount
+            <*> (symbol "CASH ALTERNATIVES INTEREST" *> parseSymbol)
+        )
+    <|> ( CourtesyAdjustment
+            <$ ( symbol "Couresy Adjustment"
+                   <|> symbol "Courtesy Adjustment"
+                   <|> symbol "Courtesy adjustment"
+                   <|> symbol "Courteys adjustment"
+               )
+        )
+    <|> ( CourtesyCredit
+            <$ ( symbol "Courtesy Credit"
+                   <|> symbol "courtesy credit"
+               )
+        )
+    <|> (ForeignTaxWithheld <$> (symbol "FOREIGN TAX WITHHELD" *> parseSymbol))
+    <|> ( FundDisbursement
+            <$ symbol
+              "CLIENT REQUESTED ELECTRONIC FUNDING DISBURSEMENT (FUNDS NOW)"
+        )
+    <|> ( IncomingAccountTransfer
+            <$ symbol "CASH MOVEMENT OF INCOMING ACCOUNT TRANSFER"
+        )
+    <|> ( InterestAdjustment
+            <$ symbol "FREE BALANCE INTEREST ADJUSTMENT~NO DESCRIPTION"
+        )
+    <|> ( InterestIncome
+            <$> ( symbol "INTEREST INCOME - SECURITIES~"
+                    *> (TL.pack <$> manyTill (satisfy (const True)) eof)
+                )
+        )
+    <|> (MarkToMarket <$ symbol "MARK TO THE MARKET")
+    <|> (MiscellaneousJournalEntry <$ symbol "MISCELLANEOUS JOURNAL ENTRY")
+    <|> (OffCycleInterest <$> (symbol "OFF-CYCLE INTEREST~" *> parseSymbol))
+    <|> (OrdinaryDividend <$> (symbol "ORDINARY DIVIDEND~" *> parseSymbol))
+    <|> (QualifiedDividend <$> (symbol "QUALIFIED DIVIDEND~" *> parseSymbol))
+    <|> (Rebate <$ symbol "REBATE")
+    <|> ( symbol "REMOVAL OF OPTION DUE TO ASSIGNMENT"
+            *> ( RemoveOptionDueToAssignment
+                   <$> parseAmount
+                   <*> parseSymbol
+                   <*> parseOption
+               )
+        )
+    <|> ( symbol "REMOVAL OF OPTION DUE TO EXPIRATION"
+            *> ( RemoveOptionDueToExpiration
+                   <$> parseAmount
+                     <*> parseSymbol
+                     <*> parseOption
+               )
+        )
+    <|> (Sold <$> parseDevice <*> (symbol "SOLD" *> parseTrade))
+    <|> ( TransferBetweenAccounts
+            <$ symbol "INTERNAL TRANSFER BETWEEN ACCOUNTS OR ACCOUNT TYPES"
+        )
+    <|> (TransferFromForexAccount <$ symbol "TRANSFER FROM FOREX ACCOUNT")
+    <|> ( TransferInSecurityOrOption
+            <$ symbol "TRANSFER OF SECURITY OR OPTION IN"
+        )
+    <|> (TransferOfCash <$ symbol "INTERNAL TRANSFER OF CASH")
+    <|> (TransferToForexAccount <$ symbol "TRANSFER TO FOREX ACCOUNT")
+    <|> (WireIncoming <$ symbol "WIRE INCOMING")
+
+parseOption :: Parser TOSOption
+parseOption = undefined
+
+parseExchange :: Parser Text
+parseExchange =
+  asum $
+    map
+      symbol
+      [ "AMEX",
+        "ARCA",
+        "BATS",
+        "BOX",
+        "C2",
+        "CBOE",
+        "EDGX",
+        "ISE",
+        "ISE GEMINI",
+        "MIAX",
+        "NASDAQ",
+        "NASDAQ OM",
+        "NYSE",
+        "PHLX"
+      ]
+
+whiteSpace :: Parser ()
+whiteSpace = L.space space1 lineCmnt blockCmnt
+  where
+    lineCmnt = L.skipLineComment "#"
+    blockCmnt = L.skipBlockComment "/*" "*/"
+
+lexeme :: Parser a -> Parser a
+lexeme p = p <* whiteSpace
+
+symbol :: Text -> Parser Text
+symbol = lexeme . string
+
+testParser :: Text -> Either (ParseErrorBundle Text Void) TOSEntry
+testParser = parse parseEntry ""
