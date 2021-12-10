04a8a092794142bc0809e071f104b10fa8dbf96a
John Wiegley <johnw@newartisans.com>
2021-12-10 11:24:53 -0800

    Reorganize ICP.hs a bit

--- src/Journal/Crypto/ICP.hs
+++ src/Journal/Crypto/ICP.hs
@@ -38,92 +38,73 @@ type AccountId = Text
 
 type NeuronId = Int64
 
-data ICP
-  = Stake
-      { _stakeNeuron :: NeuronId,
-        _stakeAmount :: Amount 8
-      }
-  | Refresh
-      { _refreshNeuron :: NeuronId,
-        _refreshAmount :: Amount 8
-      }
-  | AccrueMaturity
-      { -- usually inferred
-        _accrueMaturityNeuron :: NeuronId,
-        _accrueMaturityAmount :: Amount 8
-      }
-  | MergeMaturity
-      { _mergeMaturityNeuron :: NeuronId,
-        _mergeMaturityAmount :: Amount 2
-      }
-  | Spawn
-      { _spawnNeuron :: NeuronId,
-        _spawnCreated :: NeuronId
-      }
-  | Disburse
-      { _disburseNeuron :: NeuronId,
-        _disburseAmount :: Amount 8
-      }
-  | Split
-      { _splitNeuron :: NeuronId,
-        _splitCreated :: NeuronId
-      }
-  | Merge
-      { _mergeTarget :: NeuronId,
-        _mergeSource :: NeuronId
-      }
+data ManageNeuron
+  = Stake {_stakeAmount :: Amount 8}
+  | Refresh {_refreshAmount :: Amount 8}
+  | AccrueMaturity {_accrueMaturityAmount :: Amount 8}
+  | MergeMaturity {_mergeMaturityAmount :: Amount 2}
+  | Spawn {_spawnedNeuron :: NeuronId}
+  | Disburse {_disburseAmount :: Amount 8}
+  | Split {_splitNeuron :: NeuronId}
+  | Merge {_mergeSource :: NeuronId}
   deriving (Show, PrettyVal, Eq, Generic)
 
-makePrisms ''ICP
+makePrisms ''ManageNeuron
+
+data ICP = ICP
+  { _neuron :: NeuronId,
+    _manage :: ManageNeuron
+  }
+  deriving (Show, PrettyVal, Eq, Generic)
+
+makeLenses ''ICP
 
 -- | A fold over ICP transactions that yields the associated network fees.
 --   These are constant for the Internet Computer and not dependent on the
 --   transaction.
-icpFees :: Fold ICP (Amount 6)
-icpFees f =
-  fmap (error "Never reached") . f . \case
-    Stake {} -> transactionFee
-    Refresh {} -> transactionFee
-    AccrueMaturity {} -> 0
-    MergeMaturity {} -> 0
-    Spawn {} -> transactionFee
-    Disburse {} -> transactionFee
-    Split {} -> transactionFee
-    Merge {} -> transactionFee
+icpFees :: ICP -> Amount 6
+icpFees icp = case icp ^. manage of
+  Stake {} -> transactionFee
+  Refresh {} -> transactionFee
+  AccrueMaturity {} -> 0
+  MergeMaturity {} -> 0
+  Spawn {} -> transactionFee
+  Disburse {} -> transactionFee
+  Split {} -> transactionFee
+  Merge {} -> transactionFee
 
-_ICPNetAmount :: Fold ICP (Amount 2)
-_ICPNetAmount f =
-  fmap (error "Never reached") . f . view coerced . \case
-    Stake _ amt -> - amt
-    Refresh _ amt -> - amt
-    AccrueMaturity {} -> 0
-    MergeMaturity {} -> 0
-    Spawn {} -> 0
-    Disburse _ amt -> amt
-    Split {} -> 0
-    Merge {} -> 0
+icpNetAmount :: ICP -> Amount 2
+icpNetAmount icp = case icp ^. manage of
+  Stake amt -> - amt ^. coerced
+  Refresh amt -> - amt ^. coerced
+  AccrueMaturity {} -> 0
+  MergeMaturity {} -> 0
+  Spawn {} -> 0
+  Disburse amt -> amt ^. coerced
+  Split {} -> 0
+  Merge {} -> 0
 
 instance HasNetAmount (Const ICP) where
-  _NetAmount f (Const s) = fmap Const $ s & _ICPNetAmount %%~ f
+  _NetAmount f (Const icp) = error "never used" <$> f (icpNetAmount icp)
 
 printICP :: ICP -> TL.Text
-printICP = \case
-  Stake n amt ->
+printICP (ICP n m) = case m of
+  Stake amt ->
     "stake " <> tshow n <> " " <> printAmount 8 amt
-  Refresh n amt ->
+  Refresh amt ->
     "refresh " <> tshow n <> " " <> printAmount 8 amt
-  AccrueMaturity n amt ->
+  AccrueMaturity amt ->
     "accrue maturity " <> tshow n <> " " <> printAmount 8 amt
-  MergeMaturity n amt ->
+  MergeMaturity amt ->
     "merge maturity " <> tshow n <> " " <> printAmount 8 amt
-  Spawn n1 n2 ->
-    "spawn " <> tshow n1 <> " -> " <> tshow n2
-  Disburse n amt ->
+  Spawn n2 ->
+    "spawn " <> tshow n <> " -> " <> tshow n2
+  Disburse amt ->
     "disburse " <> tshow n <> " " <> printAmount 8 amt
-  Split n1 n2 ->
-    "split " <> tshow n1 <> " -> " <> tshow n2
-  Merge n1 n2 ->
-    "merge " <> tshow n1 <> " <- " <> tshow n2
+  Split n2 ->
+    "split " <> tshow n <> " -> " <> tshow n2
+  Merge n2 ->
+    "merge " <> tshow n <> " <- " <> tshow n2
   where
     tshow = TL.pack . show
 
@@ -133,33 +114,33 @@ instance Printable (Const ICP) where
 parseICP :: Parser ICP
 parseICP = do
   keyword "stake"
-    *> (Stake <$> (L.decimal <* whiteSpace) <*> parseAmount)
+    *> (ICP <$> (L.decimal <* whiteSpace) <*> (Stake <$> parseAmount))
     <|> keyword "refresh"
-      *> (Refresh <$> (L.decimal <* whiteSpace) <*> parseAmount)
+      *> (ICP <$> (L.decimal <* whiteSpace) <*> (Refresh <$> parseAmount))
     <|> keyword "accrue maturity"
-      *> (AccrueMaturity <$> (L.decimal <* whiteSpace) <*> parseAmount)
+      *> (ICP <$> (L.decimal <* whiteSpace) <*> (AccrueMaturity <$> parseAmount))
     <|> keyword "merge maturity"
-      *> (MergeMaturity <$> (L.decimal <* whiteSpace) <*> parseAmount)
+      *> (ICP <$> (L.decimal <* whiteSpace) <*> (MergeMaturity <$> parseAmount))
     <|> keyword "spawn"
-      *> (Spawn <$> (L.decimal <* whiteSpace) <*> L.decimal)
+      *> (ICP <$> (L.decimal <* whiteSpace) <*> (Spawn <$> L.decimal))
     <|> keyword "disburse"
-      *> (Disburse <$> (L.decimal <* whiteSpace) <*> parseAmount)
+      *> (ICP <$> (L.decimal <* whiteSpace) <*> (Disburse <$> parseAmount))
     <|> keyword "split"
-      *> (Split <$> (L.decimal <* whiteSpace) <*> L.decimal)
+      *> (ICP <$> (L.decimal <* whiteSpace) <*> (Split <$> L.decimal))
     <|> keyword "merge"
-      *> (Merge <$> (L.decimal <* whiteSpace) <*> L.decimal)
+      *> (ICP <$> (L.decimal <* whiteSpace) <*> (Merge <$> L.decimal))
 
 instance Producible Parser (Const ICP) where
   produce = fmap Const parseICP
 
 _ICPLedgerRepr :: Fold (Annotated ICP) (Transaction (Annotated ICP) 8)
 _ICPLedgerRepr f ann =
-  fmap (error "Never reached") . f $ case ann ^. item of
-    Stake _n _amt -> undefined
-    Refresh _n _amt -> undefined
-    AccrueMaturity _n _amt -> undefined
-    MergeMaturity _n _amt -> undefined
-    Spawn _n1 _n2 -> undefined
-    Disburse _n _amt -> undefined
-    Split _n1 _n2 -> undefined
-    Merge _n1 _n2 -> undefined
+  fmap (error "Never reached") . f $ case ann ^. item . manage of
+    Stake _amt -> undefined
+    Refresh _amt -> undefined
+    AccrueMaturity _amt -> undefined
+    MergeMaturity _amt -> undefined
+    Spawn _n2 -> undefined
+    Disburse _amt -> undefined
+    Split _n2 -> undefined
+    Merge _n2 -> undefined
