5ee2c86d2cb7b925ff985f337b608a7dc1703fcb
John Wiegley <johnw@newartisans.com>
2024-05-04 08:11:12 -0700

    Update to build with GHC 9.8.2

--- flake.nix
+++ flake.nix
@@ -21,7 +21,8 @@
           trade-journal =
             final.haskell-nix.project' {
               src = ./.;
-              compiler-nix-name = "ghc963";
+              supportHpack = true;
+              compiler-nix-name = "ghc982";
               shell.tools = {
                 cabal = {};
                 haskell-language-server = {};
